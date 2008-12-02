da8ba2d481e8000eeb385b7ecd273ec88d0c96a5
Christopher Wellons <mosquitopsu@gmail.com>
2008-12-02 00:53:32 -0500

    Modified algorithm to fill in tighter locations first.
    With this modification, it can solve a 100x100 board in less time than
    the original could solve an 8x8.

--- knight.c
+++ knight.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #define XSIZE 8
 #define YSIZE 8
@@ -12,6 +13,7 @@ int moves[8][2] = {
 void print_board ();
 int valid (int x, int y);
 int solve_board (int x, int y, int n);
+int compar (const void *a, const void *b);
 
 int main ()
 {
@@ -41,21 +43,39 @@ int valid (int x, int y)
     board[y][x] == 0;
 }
 
+int compar (const void *a, const void *b)
+{
+  return ((int *) a)[0] - ((int *) b)[0];
+}
+
 int solve_board (int x, int y, int n)
 {
   board[y][x] = n;
   if (n == XSIZE * YSIZE)
     return 1;
 
-  int i;
+  /* Gather valid moves and count their "loneliness". */
+  int i, j, valids[8][3];
   for (i = 0; i < 8; i++)
     {
-      int xx = x + moves[i][0];
-      int yy = y + moves[i][1];
-      if (valid (xx, yy) && solve_board (xx, yy, n + 1))
-	return 1;
+      valids[i][0] = 0;
+      valids[i][1] = x + moves[i][0];
+      valids[i][2] = y + moves[i][1];
+      if (valid (valids[i][1], valids[i][2]))
+	for (j = 0; j < 8; j++)
+	  if (valid (valids[i][1] + moves[j][0],
+		     valids[i][2] + moves[j][1]))
+	    valids[i][0]++;
     }
 
+  /* Sort and try each one. */
+  qsort (&valids[0][0], 8, 3 * sizeof (int), &compar);
+  for (i = 0; i < 8; i++)
+    if (valid (valids[i][1], valids[i][2]) &&
+	solve_board (valids[i][1], valids[i][2], n + 1))
+      return 1;
+
+  /* Dead end: back off. */
   board[y][x] = 0;
   return 0;
 }
